62265c699108cc48f33fc1524217c295f4dcb78e
Kai <fushimi.kai@aiotcloud.co.jp>
2023-05-24 17:59:14 +0900

    2023/05/24 18:00

--- 競プロ典型90問/002/002 - Encyclopedia of Parentheses.py
+++ 競プロ典型90問/002/002 - Encyclopedia of Parentheses.py
@@ -1,10 +1,34 @@
-N = int(input())
+import sys
+N = int(sys.stdin.readline())
+
 ans = []
 
 for i in range(2**N):
-    kakko = []
+    kakko = [0] * N
+    cnt = 0
     for j in range(N):
-        if (i >> j) & 1:
-            kakko.insert(-j, '(')
+        if cnt < 0:
+            break
         else:
-            kakko.insert(-j, ')')
+            if (i >> j) & 1:
+                kakko[j] = '1'
+                cnt += 1
+            else:
+                kakko[j] = '0'
+                cnt -= 1
+
+    if cnt == 0:
+        ans.append(int(''.join(kakko)))
+    else:
+        pass
+
+ans.sort(reverse=True)
+
+for k in ans:
+    k = list(str(k))
+    for l in range(len(k)):
+        if k[l] == '1':
+            k[l] = '('
+        else:
+            k[l] = ')'
+    print(''.join(k))
