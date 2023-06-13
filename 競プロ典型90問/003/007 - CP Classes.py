c6b5f0ede84f69f2b28ef40b5a16ef95526ba1fa
Kai <fushimi.kai@aiotcloud.co.jp>
2023-06-13 17:08:39 +0900

    2023/06/13 17:10

---
+++ 競プロ典型90問/003/007 - CP Classes.py
@@ -0,0 +1,34 @@
+N = int(input())
+A = sorted(list(map(int,input().split())))
+Q = int(input())
+B = []
+for b in range(1,Q+1):
+    B.append(int(input()))
+
+def binary_search(data, value):
+    left = 0
+    right = len(data) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if data[mid] == value:
+            return 0
+        elif data[mid] < value:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    if mid == 0:
+        if len(data) == 1:
+            return abs(data[0]-value)
+        else:
+            return min(abs(data[0]-value),abs(data[1]-value))
+    elif mid == len(data)-1:
+        return min(abs(data[-1]-value),abs(data[-2]-value))
+    else:
+        return min(
+            min(abs(data[mid-1]-value),abs(data[mid]-value)),
+            min(abs(data[mid]-value),abs(data[mid+1]-value))
+        )
+
+for b in B:
+    print(binary_search(A,b))
